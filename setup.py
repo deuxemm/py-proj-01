442d569f680788fed13743485ef751569bbedc9a
deuxemm <deuxemm@vivaldi.net>
2023-05-03 14:37:28 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptool import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup (
+    name='pgbackup',
+    version='0.1.0',
+    author='Marc McRae',
+    author_email = 'deuxemm@vivaldi.net',
+    description = 'A utility for backing up PostgreSQL databases.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://deuxemm.github.com/pgbackup',
+    packages=find_packages('src')
+)
+
